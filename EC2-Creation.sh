e6510abceeffc60cc2561eadc495880f4cb26c77
Jebin1991s <jebin1991s@gmail.com>
2026-03-07 17:32:23 +0530

    Add EC2 instance creation script

---
+++ EC2-Creation.sh
@@ -0,0 +1,5 @@
+AMIID=$1
+InstanceType=$2
+Keyname=$3
+Security_Group=$4
+aws ec2 run-instances --image-id $AMIID --count 1 --instance-type $InstanceType --key-name $Keyname --security-groups $Security_Group
